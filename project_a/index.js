011db2209312c7cce04e99c19e2656f8f610f6ee
higekick <yutoikegami@gmail.com>
2018-03-08 22:36:29 +0900

    first commit

---
+++ project_a/index.js
@@ -0,0 +1,11 @@
+const colors = require('colors')
+
+const s1 = 'A time to throw stones arway'
+const s2 = 'and a time to gather stone together'
+const s3 = 'A time to search and time to give up as lost'
+const s4 = 'A time to keep and a time to throw away'
+
+console.log(s1.underline.red)
+console.log(s2.inverse.blue)
+console.log(s3.rainbow)
+console.log(s4.inverse.red)
